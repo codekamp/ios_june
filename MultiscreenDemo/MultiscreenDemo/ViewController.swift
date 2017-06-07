06ba276f9197ebbe515f1d360f5c9cb53bb62180
Prashant Chaudhary <101.prashant@gmail.com>
2017-06-07 15:15:04 +0530

    Multiscreen demo

---
+++ MultiscreenDemo/MultiscreenDemo/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  MultiscreenDemo
+//
+//  Created by Cerebro on 07/06/17.
+//  Copyright © 2017 CodeKamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var firstNumField: UITextField!
+
+    @IBOutlet weak var secondNumField: UITextField!
+    
+    
+    @IBAction func add(_ sender: Any) {
+        let result = Int(firstNumField.text!)! + Int(secondNumField.text!)!
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let vc = storyboard.instantiateViewController(withIdentifier: "result_vc") as! ResultViewController
+        
+        vc.result = result
+        
+        present(vc, animated: false, completion: nil)
+    }
+}
+
