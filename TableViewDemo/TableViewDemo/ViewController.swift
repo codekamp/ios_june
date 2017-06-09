f43bc4dc408b8ff33af8fe37082e8b63da4e0520
Prashant Chaudhary <101.prashant@gmail.com>
2017-06-09 21:29:50 +0530

    tableview demo

---
+++ TableViewDemo/TableViewDemo/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by Cerebro on 09/06/17.
+//  Copyright © 2017 CodeKamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
+    @IBOutlet weak var myTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        myTableView.dataSource = self
+        myTableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 150;
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "my_cell_identifier")!
+        
+        cell.textLabel?.text = "\(101 + indexPath.row)"
+        
+        print("called for row \(indexPath.row)")
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("cell clicked: \(indexPath.row)")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
