fa1e9d897656764a0638591a2d9ac444e7430aab
Prashant Chaudhary <101.prashant@gmail.com>
2017-06-20 20:21:37 +0530

    Autolayout and Post

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Cerebro on 20/06/17.
+//  Copyright © 2017 CodeKamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
